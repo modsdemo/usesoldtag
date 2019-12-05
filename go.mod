336b78aa4342ffa7f9965381fa98803831e9a7d8
Pontus Leitzler <pontus.leitzler@zebware.com>
2019-12-05 12:44:35 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/modsdemo/usesoldtag
+
+go 1.13
+
+require github.com/modsdemo/exportedtag v0.0.2
---
+++ usesoldtag.go
@@ -0,0 +1,8 @@
+// Package usesoldtag ..
+package usesoldtag
+
+import "github.com/modsdemo/exportedtag"
+
+func Deptag() string {
+	return exportedtag.Tag
+}
